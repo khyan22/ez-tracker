4d8fdc6f46b834707b6146b822bd7340582555aa
khyan22 <khyanalvarez@gmail.com>
2022-06-02 01:55:03 -0400

    added getAllRoles()

--- app.js
+++ app.js
@@ -1,4 +1,52 @@
 const db = require('./db/connection');
+const index = require('./utils/index');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
 
+db.connect(err => {
+    if (err) throw err
+});
+
+// Welcome message
+console.log(`
+|============================|
+|                            |
+|    Welcome to EZ{Tracker}  |  
+|                            |
+|============================|
+`)
+
+const app = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'startUp',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee',
+                "Update an employee's manager",
+                'View employee by manager',
+                'View employee by department',
+                'View total salary expenditure'
+            ]
+        }
+    ]).then(choice => {
+        switch (choice.startUp) {
+            case 'View all departments':
+                index.getAllDepartments()
+                break;
+            case 'View all roles':
+                index.getAllRoles()
+                break
+        }
+    })
+};
+
+app()
+
+module.exports = {app}
